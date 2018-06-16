6780cd7214e4c727fdc72412263f2d6ab4af7ff5
Balázs Úr <urbalazs@gmail.com>
2018-06-16 18:23:34 +0200

    Updated Hungarian translation

---
+++ Locale/hu_HU/translations.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'Sign up' => 'Regisztráció',
+    'Unable to create your account' => 'Nem lehet létrehozni a fiókját',
+    'activate my account' => 'fiókom aktiválása',
+    'Click on this link to activate your account:' => 'Kattintson erre a hivatkozásra a fiók aktiválásához:',
+    'Hello %s' => 'Üdvözöljük %s',
+    'Email domain restriction for sign up' => 'E-mail tartomány korlátozás a regisztrációhoz',
+    'Only people with this email address will be allowed to sign up.' => 'Csak az ezzel az e-mail címmel rendelkező emberek jogosultak regisztrálni.',
+    'Send email verification' => 'E-mail megerősítés küldése',
+    'Email verification' => 'E-mail megerősítés',
+    'You are not allowed to register' => 'Önnek nem engedélyezett regisztrálni',
+    'Allow people to sign up themselves on Kanboard' => 'Lehetővé teszi az embereknek, hogy önmaguk regisztráljanak a Kanboardra',
+);

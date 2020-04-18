718698fb6fc18e3097ecff034ce1c989262a5dae
trendspotter <j.podhorecky@volny.cz>
2020-04-18 18:01:24 +0200

    Add cs_CZ translations

---
+++ Locale/cs_CZ/translations.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'Sign up' => 'Přihlásit se',
+    'Unable to create your account' => 'Účet nelze vytvořit',
+    'activate my account' => 'aktivovat můj účet',
+    'Click on this link to activate your account:' => 'Kliknutím na tento odkaz aktivujte svůj účet:',
+    'Hello %s' => 'Dobrý den %s',
+    'Email domain restriction for sign up' => 'Omezení e-mailové domény pro registraci',
+    'Only people with this email address will be allowed to sign up.' => 'Registrovat se budou moci pouze lidé s touto e-mailovou doménou.',
+    'Send email verification' => 'Odeslat ověření e-mailem',
+    'Email verification' => 'Potvrzení e-mailu',
+    'You are not allowed to register' => 'Nemáte oprávnění k registraci',
+    'Allow people to sign up themselves on Kanboard' => 'Umožnit lidem přihlásit se na Kanboard',
+);
